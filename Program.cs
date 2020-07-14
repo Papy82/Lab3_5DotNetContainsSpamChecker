f00ebd7795fc6b6ed1f82f950f0b9cb9bc73dc7b
Papy82 <62964929+Papy82@users.noreply.github.com>
2020-07-14 08:37:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab3_5DotNetContainsSpamChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World Lab3_5DotNetContainsSpamChecker!");
+
+            string blackListWord = "Fudge";
+            bool isSpam = false;
+
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam.");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+
+        }
+    }
+}
